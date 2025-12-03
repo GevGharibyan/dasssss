4009590204475bce8ed600a2bafc5a315228d9e3
Gev <gharibyangev55@gmail.com>
2025-12-03 19:25:34 +0400

    3 commitik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<div className=" text-center text-9xl pt-5">
+<div className="h-screen w-full bg-orange-500 flex justify-center items-center ">
+<div className=" text-center text-9xl pt-5 bg-blue-700 ">
   Gev Gharibyan
+</div>
 </div>
   );
 }
